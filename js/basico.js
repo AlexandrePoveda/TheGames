06da83a35c5ce9f197a439faf96b6560609e1189
Alexandre Poveda <alexandrepoveda21@gmail.com>
2025-10-03 11:55:02 -0300

    Iniciando a seção de cards do game - Importando o arquivo de objeto - para o Javascript

---
+++ js/basico.js
@@ -0,0 +1,17 @@
+// mensagem no console e no alert
+console.log('Usando Javascript');
+console.log('fsdffsdf');
+// alert('Usando Javascript');
+ 
+// variáveis
+let nomeGame = 'God of War';
+let anoLancamento = 2005;
+let finalizado = true;
+ 
+// exibir as variáveis
+console.log(nomeGame);
+console.log(anoLancamento);
+console.log(finalizado);
+ 
+// concatenação não é legal em Javascript
+console.log('Nome do game: ' + nomeGame + ' Finalizado: ' + finalizado);
---
+++ js/montarFrontEnd.js
@@ -0,0 +1,5 @@
+//importar os objetos que serão manipulados no front-end
+import { games } from "./objetos.js";
+
+// exibir no console
+console.log(games);
